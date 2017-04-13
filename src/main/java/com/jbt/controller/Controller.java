6791ada4de00d1a3ca13d31a85a90d898638ca0e
Admin <admin@javabeginnerstutorial.com>
2017-04-13 20:40:58 +0200

    First Commit

---
+++ src/main/java/com/jbt/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.jbt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    String HelloWorld(){
+        return "Hello World";
+    }
+}
