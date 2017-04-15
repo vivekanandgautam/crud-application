8329b3d55030279a2ae736a6593dd88066bf2659
Admin <admin@javabeginnerstutorial.com>
2017-04-15 21:27:33 +0200

    DB Changes

--- src/main/java/com/jbt/controller/Controller.java
+++ src/main/java/com/jbt/controller/Controller.java
@@ -1,13 +1,19 @@
 package com.jbt.controller;
 
+import com.jbt.service.MySqlDb;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+    @Autowired
+    MySqlDb mySqlDb;
+
     @RequestMapping("/")
     String HelloWorld(){
+        mySqlDb.method();
         return "Hello World";
     }
 }
